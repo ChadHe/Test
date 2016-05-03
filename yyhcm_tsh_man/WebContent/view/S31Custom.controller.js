2924f9f1413688864d146ce55ba6a02711aceae3
chadhe <he_changqing@hotmail.com>
2016-05-03 15:51:46 +0800

    Update

--- yyhcm_tsh_man/WebContent/view/S31Custom.controller.js
+++ yyhcm_tsh_man/WebContent/view/S31Custom.controller.js
@@ -1,19 +1,35 @@
 sap.ui.controller("hcm.mytimesheet.yyhcm_tsh_man.view.S31Custom", {
 	onInit: function(){
-		var date = new Date();
-		var oODataModel;
-		oODataModel = new sap.ui.model.odata.ODataModel("proxy/sap/opu/odata/sap/YYCATS_SRV/");
-//		oODataModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
-		this.getView().setModel(oODataModel,"WTAssign");
-		this.getView().byId("worktimeAssignmentPanel").bindElement("WTAssign>/WorktimeAssignSet(datetime'2016-04-29T00:00:00')");
+		var oModelexch = this.oApplication.getModel("S31modelexch");
+		if (oModelexch){
+			var oODataModel = new sap.ui.model.odata.ODataModel("proxy/sap/opu/odata/sap/YYCATS_SRV/");
+			this.getView().setModel(oODataModel,"WTAssign");
+			this.getView().getModel("WTAssign").setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+
+			var dSelectedDate = new Date(oModelexch.getProperty("/selectedDates")[0]);
+			var sDateString = dSelectedDate.getFullYear()
+							+ '-'
+							+ dSelectedDate.getMonth()
+							+ '-'
+							+ dSelectedDate.getDate()
+							+ 'T00:00:00';
+			var sDatePath = "WTAssign>/WorktimeAssignSet(datetime'" + sDateString + "')";
+			this.getView().byId("worktimeAssignmentPanel").bindElement(sDatePath);
+		}
 	},
 	
 	saveWorkAssign: function(){
 		var oService = this.getView().getModel("WTAssign");
-		var date = new Date();
-		oService.update("/WorktimeAssignSet(datetime'2016-04-29T00:00:00')",{
-				Keydate: date,
-				RangeStart: date.getTime()
-			});
+		var oModelexch = this.oApplication.getModel("S31modelexch");
+		var dSelectedDate = new Date(oModelexch.getProperty("/selectedDates")[0]);
+		var sDateString = dSelectedDate.getFullYear()
+						+ '-'
+						+ dSelectedDate.getMonth()
+						+ '-'
+						+ dSelectedDate.getDate()
+						+ 'T00:00:00';
+		var sDatePath = "WTAssign>/WorktimeAssignSet(datetime'" + sDateString + "')";
+		
+		oService.update(sDatePath,this.getView().getModel("WTAssign").oData);
 	}
 });
