20631df6bfdfaad8f366e1847851dc89cfb4da36
chadhe <he_changqing@hotmail.com>
2016-05-11 18:22:02 +0800

    Upload

--- yyhcm_tsh_man/WebContent/view/S31Custom.controller.js
+++ yyhcm_tsh_man/WebContent/view/S31Custom.controller.js
@@ -321,5 +321,27 @@ sap.ui.controller("hcm.mytimesheet.yyhcm_tsh_man.view.S31Custom", {
 					});
 		}*/
 		//ADD End   CHAHE 2016/04/28
+	},
+	
+	handleUploadComplete: function(oEvent) {
+		var sResponse = oEvent.getParameter("response");
+		if (sResponse) {
+			var sMsg = "";
+			var m = /^\[(\d\d\d)\]:(.*)$/.exec(sResponse);
+			if (m[1] == "200") {
+				sMsg = "Return Code: " + m[1] + "\n" + m[2], "SUCCESS", "Upload Success";
+				oEvent.getSource().setValue("");
+			} else {
+				sMsg = "Return Code: " + m[1] + "\n" + m[2], "ERROR", "Upload Error";
+			}
+
+			MessageToast.show(sMsg);
+		}
+	},
+
+	handleUploadPress: function(oEvent) {
+		var oFileUploader = this.getView().byId("fileUploader");
+//		oFileUploader.setProperty("uploadUrl", this.getView().getModel("WTAssign").sServiceUrl + "/WorktimeAssignSet(datetime'2016-05-11T00:00:00')");
+		oFileUploader.upload();
 	}
 });
